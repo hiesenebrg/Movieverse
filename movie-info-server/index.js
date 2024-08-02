93b7cc05da0d6494a1056c066f2fd5e7857a4747
hiesenebrg <adarshsingh8008@gmail.com>
2024-08-02 15:03:53 +0530

    port env var added

--- movie-info-server/index.js
+++ movie-info-server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+
 const env = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const passportJWT = require("./config/passport-jwt");
 const cors = require("cors");
 const passport = require("passport");
+const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
