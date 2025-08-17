74d1777332d3a07a6e46748f15d7e8d62e16118e
Adarsh Singh <93775283+hiesenebrg@users.noreply.github.com>
2025-08-17 16:33:00 +0530

    Update redis.js

--- movie-info-server/config/redis.js
+++ movie-info-server/config/redis.js
@@ -1,7 +1,7 @@
 const Redis = require("ioredis");
 
  const redisClient = new Redis(
-  "rediss://default:Aeo9AAIjcDFmNzU3NjNkNGQyZmY0OWE0YWFhMWIyMzE3MGZlNWE4ZXAxMA@strong-weevil-59965.upstash.io:6379"
+  "rediss://default:ARhqAAImcDE5OGU2NDk5ODAxNDU0MzRjYTQ1MmZlYTg0MmM3Njk0N3AxNjI1MA@viable-hagfish-6250.upstash.io:6379"
 );
 module.exports =redisClient;
 // await client.set('foo', 'bar');
